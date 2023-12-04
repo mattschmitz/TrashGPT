c291c8ced6bf2a0184aa5cff8b9ea00ad140b901
carlsonivanic <carlsonivanicjansen@gmail.com>
2023-12-04 00:00:42 -0800

    Semi working final code

--- esp32files/main.py
+++ esp32files/main.py
@@ -2,6 +2,10 @@ import camera
 import base64
 from machine import Pin, PWM
 from time import sleep
+import api
+import network
+import requests
+import gc
 
 floodlight = Pin(4, Pin.OUT)
 shutterbutton = Pin(13, Pin.IN, Pin.PULL_UP)
@@ -9,11 +13,13 @@ builtinled = Pin(33, Pin.OUT)
 
 takepic = False
 
-def beep(duration=0.1):
-    buzzer.duty(512)  # Activate the buzzer at half power
-    sleep(duration)   # Keep the buzzer on for the specified duration
-    buzzer.duty(0)    # Turn off the buzzer
-    sleep(0.1)        # Short pause between beeps
+def beep(times, duration=0.1):
+    for _ in range(times):
+        print("beep")
+        buzzer.duty(512)  # Activate the buzzer at half power
+        sleep(duration)   # Keep the buzzer on for the specified duration
+        buzzer.duty(0)    # Turn off the buzzer
+        sleep(0.1)        # Short pause between beeps
 
 def PlayShutterSound():
     buzzer.duty(512)  # Set volume (duty cycle)
@@ -30,30 +36,73 @@ def CheckPressed(pin):
   interrupt_pin = pin
   
 def InitializeCamera():
+    print("Initializing Camera")
     try:
         camera.init(0, format=camera.JPEG)
         camera.framesize(camera.FRAME_96X96)
         camera.quality(63)
+        print("Initializing Camera SUCCESS")
     except Exception as e:
-        for _ in range(3):
-            beep(1)
-        print("reached exception")
+        beep(3,1)
+        print("Reached Exception InitializeCamera()")
         print("Exception:", str(e))
 
+def DeinitCamera():
+    print("Deinitializing Camera")
+    try:
+        camera.deinit()
+        print("Deinitializing Camera SUCCESS")
+    except Exception as e:
+        beep(3,1)
+        print("Reached Exception DeinitCamera()")
+        print("Exception:", str(e))
+    
+def GetEncodedImage(image):
+    encodedImage = base64.b64encode(image)
+    return encodedImage
+
+def GetDecodedImage(encoded_image):
+    decodedImage = encoded_image.decode('utf-8')
+    return decodedImage
+
 def TakePicture():
+    print("Taking Photo")
     try:
         floodlight.value(1)
         buf = camera.capture()
         PlayShutterSound()
+        print("Type of captured data:", type(buf))  # Check the type of buf
         floodlight.value(0)
-        encoded_image = base64.b64encode(buf)
-        print(encoded_image.decode('utf-8')[:100])
-        print(len(buf))
+        print("Done")
     except Exception as e:
-        for _ in range(3):
-            beep(1)
-        print("reached exception")
+        beep(3,1)
+        print("Reached Exception TakePicture()")
         print("Exception:", str(e))
+    return buf
+
+def ConnectWifi():
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('The Golden Apple', 'brendanlovesfood')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+    
+def CheckTrimmedResponse(tr):
+    if tr == "recyclable" or tr == "Recyclable":
+        print("CTR got RECYCLABLE")
+        beep(1,0.5)
+    elif tr == "landfill" or tr == "Landfill":
+        print("CTR got LANDFILL")
+        beep(2,0.5)
+    elif tr == "compostable" or tr == "Compostable":
+        print("CTR got COMPOSTABLE")
+        beep(3,0.5)
+    else:
+        print("CTR got ELSE")
+        beep(3,1)
 
 # Attach interrupt
 shutterbutton.irq(trigger=Pin.IRQ_FALLING, handler=CheckPressed)
@@ -62,22 +111,44 @@ shutterbutton.irq(trigger=Pin.IRQ_FALLING, handler=CheckPressed)
 # This sleep is to prevent the program become "unstoppable"
 sleep(3)
 
+# Connect to WiFi
+ConnectWifi()
+    
 # Initialize Buzzer later to prevent unwanted noise.
 buzzer = PWM(Pin(12))
 buzzer.duty(0)
 buzzer.freq(1000)
 
-# Initialize Cam
-InitializeCamera()
-
 # If hear three beep then good to go.
-for _ in range(3):
-    beep()
+beep(3)
 
-print("loop we go!") #dev purposes
+# InitializeCamera()
+# image = TakePicture()
+# DeinitCamera()
+# encodedImage = GetEncodedImage(image)
+# decodedImage = GetDecodedImage(encodedImage)
+# response = api.CallApi(decodedImage)
+# trimmedResponse = api.TrimResponse(response)
+# print(trimmedResponse)
+# CheckTrimmedResponse(trimmedResponse)
+# gc.collect()
+
 
 while True:
     if takepic:
-        TakePicture()
+        InitializeCamera()
+        image = TakePicture()
+        DeinitCamera()
+        
+        encodedImage = GetEncodedImage(image)
+        decodedImage = GetDecodedImage(encodedImage)
+        
+        response = api.CallApi(decodedImage)
+        trimmedResponse = api.TrimResponse(response)
+        print(trimmedResponse)
+        CheckTrimmedResponse(trimmedResponse)
+        
+        gc.collect()
+
         takepic = False #reset
 
