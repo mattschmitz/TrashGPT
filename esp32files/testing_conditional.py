8f1563a58fcc4b23540c93d86a2f5c4759252def
carlsonivanic <carlsonivanicjansen@gmail.com>
2023-12-05 15:53:54 -0800

    added testing_conditional

---
+++ esp32files/testing_conditional.py
@@ -0,0 +1,9 @@
+# FOR TESTINGIN THONNY ONLY
+
+while True:
+    user_input = input("Enter 1 to take a picture, or press Enter to do nothing: ")
+    
+    if user_input == "1":
+        # do something
+    else:
+        print("No action taken. Waiting for next command.")
